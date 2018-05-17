f00c5c9d3f638a168f9703b003a6dfab52d75ad5
Elvis <elvispeng@yunify.com>
2018-05-17 22:31:12 +0800

    perf(CLI): Test changelog

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Button, Message } from '@QCFE/lego-ui'
 
 class App extends Component {
   render () {
     return (
       <div>
         <h1>Demo</h1>
+        <Button onClick={() => Message.info('ok')}>Test</Button>
       </div>
     )
   }
