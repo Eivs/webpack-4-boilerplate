3b0a86852159ec6e74cfc5cf47443ba37f2c23f9
Elvis <elvispeng@yunify.com>
2018-10-12 16:13:21 +0800

    chore: update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import '@QCFE/lego-ui/lib/scss/lego-ui.min.css';
+import { Button, Message } from '@QCFE/lego-ui';
+import '@QCFE/lego-font/main.scss';
 
-const App = () => <div>Hello!</div>;
+const notify = () => {
+  Message.info('haha');
+};
+const App = () => (
+  <div>
+    Hello!
+    <Button onClick={notify}>Button</Button>
+  </div>
+);
 export default App;
--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -50,6 +50,18 @@ const config = {
           name: '[path][name].[ext]',
         },
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/', // config as you like
+            },
+          },
+        ],
+      },
     ],
     unsafeCache: true,
   },
